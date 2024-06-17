0bf1dbc30ebfb5c31bd099f02da879297585cb03
rtmdevelopment <sundar.sree07@gmail.com>
2024-06-18 01:54:35 +0530

    axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,5 +2,5 @@ import axios from "axios";
 import config from "../env.json";
 
 export default axios.create({
-    baseURL: `${config.localEndpoint}/api`
+    baseURL: `${config.rtmWsEndpoint}/api`
 });
