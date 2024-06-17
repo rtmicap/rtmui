017476fdb6aadd9c919f5f416899dcd89b75db95
rtmdevelopment <sundar.sree07@gmail.com>
2024-06-18 01:54:02 +0530

    shipment changes

--- src/components/My Bookings/MyBookings.jsx
+++ src/components/My Bookings/MyBookings.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { Button, Descriptions, message, Modal, Space, Table, Tag, Typography } from 'antd';
+import { Button, DatePicker, Descriptions, Drawer, Form, Input, message, Modal, Select, Space, Table, Tag, Tooltip, Typography, Upload } from 'antd';
 import { useQuery } from '@tanstack/react-query';
 import axios from "../../api/axios";
+import { UploadOutlined, PlusCircleOutlined, PlusOutlined } from '@ant-design/icons';
 import {
     CheckCircleOutlined,
     ClockCircleOutlined,
@@ -9,11 +10,14 @@ import {
     ExclamationCircleOutlined,
     MinusCircleOutlined,
     SyncOutlined,
-    ReloadOutlined
+    ReloadOutlined,
+    CloseOutlined
 } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 import moment from 'moment/moment';
 import { useAuth } from '../../contexts/AuthContext';
+import { typesOfGoods, uomChoices } from './utils';
+const { TextArea } = Input;
 
 function MyBookings() {
     const getAllBookingsUrl = "/booking/getAllBooking";
@@ -141,6 +145,8 @@ function MyBookings() {
     )
 }
 
+// View details 
+
 const ViewModal = ({ isModalOpen, handleOk, handleCancel, items }) => {
     console.log("items vie", items);
 
@@ -151,7 +157,23 @@ const ViewModal = ({ isModalOpen, handleOk, handleCancel, items }) => {
     }
 
     const [isLoading, setIsLoading] = useState(false);
+    const [open, setOpen] = useState(false);
+    const [size, setSize] = useState();
+    const [form] = Form.useForm();
+    const [shipmentDateTime, setShipmentDateTime] = useState('');
     // const [items, setItems] = useState(items);
+
+    // const showDefaultDrawer = () => {
+    //     setSize('default');
+    //     setOpen(true);
+    // };
+    const showLargeDrawer = () => {
+        setSize('large');
+        setOpen(true);
+    };
+    const onClose = () => {
+        setOpen(false);
+    };
 
     const UPDATE_QUOTE_URL = "/booking/updatequote";
 
@@ -263,9 +285,94 @@ const ViewModal = ({ isModalOpen, handleOk, handleCancel, items }) => {
                 xxl: 2,
             },
             children: items.rescheduled_reason
+        },
+        authUser.CompanyId == items.hirer_company_id &&
+        {
+            label: 'Action',
+            span: {
+                xl: 2,
+                xxl: 2,
+            },
+            children: (
+                <>
+                    <div className="row">
+                        <div className="col">
+                            <Button type="primary" onClick={showLargeDrawer}>
+                                Add My Materials
+                            </Button>
+                        </div>
+                    </div>
+                </>
+            )
         }
 
     ];
+
+    const mockData = {
+        "orderid": 6,
+        "goods_status": "goods_in_transit",
+        "shipment_datetime": "2024-06-24 11:00:00",
+        "shipment_details": [
+            {
+                "typeofgoods": "raw_material",
+                "quantity": 25,
+                "description": "test",
+                "uom": "pieces",
+                "invoice": "https://b2url/rawinvoice.pdf"
+            },
+            {
+                "typeofgoods": "tools",
+                "quantity": 1,
+                "description": "tools test",
+                "uom": "pieces",
+                "invoice": "https://b2url/tools.pdf"
+            }
+        ]
+    }
+
+    const onOk = (value) => {
+        console.log('onOk: ', value);
+    };
+
+    const onFinish = (values) => {
+        values.shipment_datetime = shipmentDateTime;
+        values.goods_status = "goods_in_transit";
+        var data = [{
+            quantity: values.quantity,
+            description: values.description,
+            uom: values.uom,
+            typeofgoods: values.typeofgoods
+        }];
+        // values.shipment_details.push(data)
+
+        console.log('Success:', values);
+    };
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+
+    const props = {
+        beforeUpload: (file) => {
+            console.log("file type: ", file);
+            const isPNG = file.type === 'application/pdf';
+            if (!isPNG) {
+                message.error(`${file.name} is not a pdf file`);
+            }
+
+            const isSizeLimit = file.size / (1024 * 1024) > 2;
+            console.log("isSizeLimit: ", isSizeLimit);
+            if (isSizeLimit) {
+                message.error(`"File size is too large (max 2 MB)`);
+            }
+
+            return isPNG || isSizeLimit || Upload.LIST_IGNORE;
+        },
+        onChange: (info) => {
+            console.log(info.fileList);
+        },
+    };
+
+
 
     return (
         <>
@@ -289,6 +396,247 @@ const ViewModal = ({ isModalOpen, handleOk, handleCancel, items }) => {
                     items={quoteItems}
                 />
             </Modal>
+
+            <Drawer
+                title={`Shipment Details Update Sheet - (Hirer) - Order ID: `}
+                placement="top"
+                size={size}
+                onClose={onClose}
+                open={open}
+                extra={
+                    <Space>
+                        <Button onClick={onClose}>Cancel</Button>
+                        <Button type="primary" onClick={onClose}>
+                            Add
+                        </Button>
+                    </Space>
+                }
+            >
+                <Form
+                    name="basic"
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    autoComplete="off"
+                    layout='vertical'
+                    form={form}
+                >
+                    <div className='row'>
+                        <div className="col">
+                            <Form.Item
+                                label="Shipment Date Time"
+                                name="shipment_datetime"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please input your shipment date & time!',
+                                    },
+                                ]}
+                            >
+                                <DatePicker
+                                    showTime
+                                    onChange={(value, dateString) => {
+                                        console.log('Selected Time: ', value);
+                                        console.log('Formatted Selected Time: ', dateString);
+                                        setShipmentDateTime(dateString);
+                                    }}
+                                    onOk={onOk}
+                                    required
+                                />
+                            </Form.Item>
+                        </div>
+                    </div>
+
+                    <div className='row'>
+                        <div className="col">
+                            <Form.Item
+                                label="Type of Goods"
+                                name="typeofgoods"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please input your type of goods!',
+                                    },
+                                ]}
+                            >
+                                <Select placeholder='Choose type of goods' style={{ width: '100%' }} options={typesOfGoods} />
+                            </Form.Item>
+                        </div>
+
+                        <div className="col">
+                            <Form.Item
+                                label="Description"
+                                name="description"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please input your description!',
+                                    },
+                                ]}
+                            >
+                                {/* <Input /> */}
+                                <TextArea rows={3} placeholder="Enter your description (max: 200 words)" maxLength={200} showCount required />
+                            </Form.Item>
+                        </div>
+
+                        <div className="col">
+                            <Form.Item
+                                label="Shipment Qty"
+                                name="quantity"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please input your shipment quantity!',
+                                    },
+                                ]}
+                            >
+                                <Input placeholder='Enter shipment quantity' required />
+                            </Form.Item>
+                        </div>
+
+                        <div className="col">
+                            <Form.Item
+                                label="UOM"
+                                name="uom"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please input your UOM!',
+                                    },
+                                ]}
+                            >
+                                <Select placeholder='Choose UOM' style={{ width: '100%' }} options={uomChoices} />
+                            </Form.Item>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col">
+                            <Form.List name="shipment_details">
+                                {(fields, { add, remove }) => (
+                                    <>
+                                        {fields.map(({ key, name, ...restField }) => {
+                                            console.log("key: ", key);
+                                            console.log("name: ", name);
+                                            return (
+                                                <>
+                                                    <h6>Shipment Details - {name + 1}</h6>
+                                                    <div className='row' key={key}>
+                                                        <div className="col">
+                                                            <Form.Item
+                                                                label="Type of Goods"
+                                                                // name="typeofgoods"
+                                                                {...restField}
+                                                                name={[name, 'typeofgoods']}
+                                                                rules={[
+                                                                    {
+                                                                        required: true,
+                                                                        message: 'Please input your type of goods!',
+                                                                    },
+                                                                ]}
+                                                            >
+                                                                <Select placeholder='Choose type of goods' style={{ width: '100%' }} options={typesOfGoods} />
+                                                            </Form.Item>
+                                                        </div>
+
+                                                        <div className="col">
+                                                            <Form.Item
+                                                                label="Description"
+                                                                // name="description"
+                                                                {...restField}
+                                                                name={[name, 'description']}
+                                                                rules={[
+                                                                    {
+                                                                        required: true,
+                                                                        message: 'Please input your description!',
+                                                                    },
+                                                                ]}
+                                                            >
+                                                                {/* <Input /> */}
+                                                                <TextArea rows={3} placeholder="Enter your description (max: 200 words)" maxLength={200} showCount required />
+                                                            </Form.Item>
+                                                        </div>
+
+                                                        <div className="col">
+                                                            <Form.Item
+                                                                label="Shipment Qty"
+                                                                // name="quantity"
+                                                                {...restField}
+                                                                name={[name, 'quantity']}
+                                                                rules={[
+                                                                    {
+                                                                        required: true,
+                                                                        message: 'Please input your shipment quantity!',
+                                                                    },
+                                                                ]}
+                                                            >
+                                                                <Input placeholder='Enter shipment quantity' required />
+                                                            </Form.Item>
+                                                        </div>
+
+                                                        <div className="col">
+                                                            <Form.Item
+                                                                label="UOM"
+                                                                // name="uom"
+                                                                {...restField}
+                                                                name={[name, 'uom']}
+                                                                rules={[
+                                                                    {
+                                                                        required: true,
+                                                                        message: 'Please input your UOM!',
+                                                                    },
+                                                                ]}
+                                                            >
+                                                                <Select placeholder='Choose UOM' style={{ width: '100%' }} options={uomChoices} />
+                                                            </Form.Item>
+                                                        </div>
+                                                        <div className='col-auto mt-4'>
+                                                            <Tooltip title={`Remove Shipment - ${name + 1}`}>
+                                                                <Button danger shape="circle" onClick={() => remove(name)} icon={<CloseOutlined />} />
+                                                            </Tooltip>
+                                                        </div>
+                                                    </div>
+                                                </>
+                                            )
+                                        }
+
+                                        )}
+                                        {/* maximum four fields can be use so total 5 details */}
+                                        {fields.length < 4 && (
+                                            <div className='row'>
+                                                <div className="col-auto">
+                                                    <Form.Item>
+                                                        <Button type="primary" onClick={() => add()} block icon={<PlusOutlined />}>
+                                                            Add more shipment details
+                                                        </Button>
+                                                        {/* <button className='btn btn-secondary'>+ Add more shipment details</button> */}
+                                                    </Form.Item>
+                                                </div>
+                                            </div>
+
+                                        )}
+                                    </>
+                                )}
+                            </Form.List>
+                            {/* <Button type='secondary' icon={<PlusOutlined />}>Add More Shipment Details</Button> */}
+                        </div>
+                    </div>
+                    <hr />
+                    <div className="row">
+                        <div className="col text-center">
+                            <Upload {...props}>
+                                <Button type='primary' icon={<PlusCircleOutlined />}>Attach Invoice</Button>
+                                <p>Max: 2 MB (Only PDF Format)</p>
+                            </Upload>
+                        </div>
+                    </div>
+                    <hr />
+                    <div className="row">
+                        <div className="col text-center">
+                            <Button htmlType='submit' type='primary'>Submit</Button>
+                        </div>
+                    </div>
+                </Form>
+            </Drawer>
         </>
     )
 }
---
+++ src/components/My Bookings/utils.js
@@ -0,0 +1,13 @@
+export const typesOfGoods = [
+    { value: 'raw_material', label: 'Raw Material' },
+    { value: 'fixtures', label: 'Fixtures' },
+    { value: 'tools', label: 'Tools' },
+    { value: 'gauges', label: 'Gauges' }
+];
+
+export const uomChoices = [
+    { value: 'nos', label: 'Nos' },
+    { value: 'kgs', label: 'Kgs' },
+    { value: 'meters', label: 'Meters' },
+    { value: 'pieces', label: 'Pieces' }
+];
