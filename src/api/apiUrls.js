20c9840c97dba47484782f35317a7d0fa390fa6a
grvinothkumar <grvinothkumar@gmail.com>
2025-02-23 02:43:12 -0500

    buy tools summary-search-detail

--- src/api/apiUrls.js
+++ src/api/apiUrls.js
@@ -52,3 +52,4 @@ export const ADMIN_PASSWORD_CHANGE = "/users/updatepassword";
 
 //tools configuration
 export const SEARCH_TOOLS = "/tools/searchtools";
+export const SEARCH_SUGGESTIONS = "/tools/searchsuggestions";
