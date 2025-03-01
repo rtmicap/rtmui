5b2019d3339db8cf2e5a84e2041fb63b1271dedb
grvinothkumar <grvinothkumar@gmail.com>
2025-03-01 12:26:20 -0500

    API changes fix

--- src/components/Tools/BuyTools.jsx
+++ src/components/Tools/BuyTools.jsx
@@ -11,64 +11,38 @@ import {firstChrUpperCase} from "../../utils/utils.js";
 function BuyTools() {
     const location = useLocation();
     const navigate = useNavigate();
+    
     const [buyTools,setBuyTools]=useState([]);
+    
+   
+
     const [searchParam,setSearchParam]=useState("");
     let previousSearch = localStorage.getItem('searchParam');
     const getAllTools = async () => {
         try {
+            if(previousSearch!="" && searchParam.trim()!="") {
             const token = localStorage.getItem('authToken');
             axios.defaults.headers.common['authorization'] = 'Bearer ' + token;
             const response = await axios.get(SEARCH_TOOLS, { params: { query: previousSearch || searchParam}});
-            if (response && response.data.result) {
-                console.log(response.data.result);
-                setBuyTools(response.data.result);
+            if (response && response.data.results) {
+                console.log(response.data.results);
+                setBuyTools(response.data.results);
                 return response.data.results;
+            }
             }
         } catch (error) {
             message.error("Error on loading orders..");
             navigate('/login');
         }
-    };
+    }
+    
 
     useEffect(() => {
+        searchParam?previousSearch=searchParam:setSearchParam(previousSearch);
         getAllTools();
         console.log(searchParam, "  buy tools");
-        previousSearch=searchParam;
     }, [searchParam]);
     
-    /* const buyTools =
-        [
-            {
-                id: 0,
-                img: "",
-                price: 120.99,
-                description: "Drill Bit set"
-            },
-            {
-                id: 1,
-                img: "",
-                price: 121.99,
-                description: "Cutting band saw"
-            },
-            {
-                id: 2,
-                img: "",
-                price: 122.99,
-                description: "Turnning mill"
-            },
-            {
-                id: 3,
-                img: "",
-                price: 123.99,
-                description: "Turnning mill"
-            },
-            {
-                id: 4,
-                img: "",
-                price: 123.99,
-                description: "Turnning mill"
-            }
-        ] */
     const cardClick = (item) => {
         console.log("card clicked ", item);
         navigate("/buytools-detail",{
@@ -83,7 +57,7 @@ function BuyTools() {
         <div>
             <h1>BuyTools</h1>
             <div className="container-layout">
-                <div className="sidebar-filter">Search/Filter <div>
+                <div className="sidebar-filter">Filter <div>
                     <SearchTools searchParam={searchParam} setSearchParam={setSearchParam}/></div> </div>
                 <div className="main-container">
                     <div>
@@ -96,7 +70,7 @@ function BuyTools() {
                                     <div className="child-card-img">
                                         <img className="card-img" src={item.tool_image[0]} alt={index} />
                                     </div>
-                                    <div className="card-price-tag">${item.tool_selling_price}</div>
+                                    <div className="card-price-tag">₹ {item.tool_selling_price}</div>
                                     <div className="card-desc"><span className="detailsCardSpan">{firstChrUpperCase(item.tool_name)}</span></div>
                                     <div className="card-desc">{firstChrUpperCase(item.tool_description)}</div>
                                     <div className="card-desc"><span className="detailsCardSpan">Condition : </span>{item.tool_condition.toUpperCase()}</div>
